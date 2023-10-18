cdafce926ec9af7db1d8dfa43ea19eba08baca9b
Jamal Ali <jamalali81@gmail.com>
2023-10-18 02:28:50 +0100

    CSV creation added

--- app/Http/Controllers/Webhooks/ProductsController.php
+++ app/Http/Controllers/Webhooks/ProductsController.php
@@ -30,36 +30,6 @@ class ProductsController extends Controller
 
         ProcessIncomingProducts::dispatch($cacheKey);
 
-        // foreach($results as $result) {
-        //     $productContent = $result->Product;
-        //     $product = json_decode($productContent);
-
-        //     unset($product->id);
-        //     unset($product->created_at);
-        //     unset($product->updated_at);
-        //     unset($product->published_at);
-        //     unset($product->handle);
-
-        //     data_forget($product, 'variants.*.id');
-        //     data_forget($product, 'variants.*.product_id');
-        //     data_forget($product, 'variants.*.created_at');
-        //     data_forget($product, 'variants.*.updated_at');
-
-        //     data_forget($product, 'options.*.id');
-        //     data_forget($product, 'options.*.product_id');
-
-        //     data_forget($product, 'images.*.id');
-        //     data_forget($product, 'images.*.product_id');
-        //     data_forget($product, 'images.*.created_at');
-        //     data_forget($product, 'images.*.updated_at');
-        //     data_forget($product, 'images.*.variant_ids');
-
-        //     $cacheKey = 'incoming_product_' . $product->title;
-        //     Cache::put($cacheKey, $product);
-
-        //     ProcessIncomingProduct::dispatch($cacheKey);
-        // }
-
         return response()->json([
             'message' => 'success'
         ], 201);
--- app/Jobs/ProcessIncomingProducts.php
+++ app/Jobs/ProcessIncomingProducts.php
@@ -24,35 +24,142 @@ class ProcessIncomingProducts implements ShouldQueue
 
         Log::info('Processing ' . count($results) . ' products.');
 
+        $storagePath = storage_path('app/public/product_data/');
+        $fileName = "products_" . time() . ".csv";
+        $filePath = $storagePath.$fileName;
+        $file = fopen($filePath, 'w');
+
+        $colHeadings = array(
+            'Command',
+            'Handle',
+            'Title',
+            'Body HTML',
+            'Vendor',
+            'Type',
+            'Tags',
+            'Tags Command',
+            'Status',
+            'Published',
+            'Published Scope',
+            'Row #',
+            'Top Row',
+
+            'Image Type',
+            'Image Src',
+            'Image Command',
+            'Image Position',
+            'Image Width',
+            'Image Height',
+            'Image Alt Text',
+
+            'Variant Command',
+            'Option1 Name',
+            'Option1 Value',
+            'Option2 Name',
+            'Option2 Value',
+            'Option3 Name',
+            'Option3 Value',
+            'Variant Position',
+            'Variant SKU',
+            'Variant Barcode',
+            'Variant Weight',
+            'Variant Weight Unit',
+            'Variant Price',
+            'Variant Compare At Price',
+            'Variant Taxable',
+            'Variant Inventory Tracker',
+            'Variant Inventory Policy',
+            'Variant Fulfillment Service',
+            'Variant Requires Shipping',
+            'Variant Inventory Qty',
+            'Variant Inventory Adjust'
+        );
+
+        fputcsv($file, $colHeadings);
+
+        $i = 0;
+        $rowNum = 1;
+        $topRow = false;
         foreach($results as $result) {
             $productContent = is_array($result) ? $result['Product'] : $result->Product;
             $product = json_decode($productContent);
 
-            unset($product->id);
-            unset($product->created_at);
-            unset($product->updated_at);
-            unset($product->published_at);
-            unset($product->handle);
+            $topRow = true;
 
-            data_forget($product, 'variants.*.id');
-            data_forget($product, 'variants.*.product_id');
-            data_forget($product, 'variants.*.created_at');
-            data_forget($product, 'variants.*.updated_at');
+            foreach($product->variants as $variant) {
 
-            data_forget($product, 'options.*.id');
-            data_forget($product, 'options.*.product_id');
+                $line = array();
 
-            data_forget($product, 'images.*.id');
-            data_forget($product, 'images.*.product_id');
-            data_forget($product, 'images.*.created_at');
-            data_forget($product, 'images.*.updated_at');
-            data_forget($product, 'images.*.variant_ids');
+                if ($variant->price > 0) {
+                    $vPrice = $variant->price * 3.673;
+                    $vInventoryQuantity = 99;
+                    $vInventoryPolicy = 'continue';
+                } else {
+                    $vPrice = 0.00;
+                    $vInventoryQuantity = 0;
+                    $vInventoryPolicy = 'deny';
+                }
 
-            $cacheKey = 'incoming_product_' . $product->title;
-            Cache::put($cacheKey, $product);
+                $image = isset($product->images[$i]) ? $product->images[$i] : null;
 
-            ProcessIncomingProduct::dispatch($cacheKey);
+                $line[] = 'MERGE';
+                $line[] = $product->handle;
+                $line[] = $product->title;
+                $line[] = $topRow ? $product->body_html : '';
+                $line[] = $product->vendor;
+                $line[] = $product->product_type;
+                $line[] = $product->tags;
+                $line[] = 'REPLACE';
+                $line[] = 'Active';
+                $line[] = 'TRUE';
+                $line[] = 'web';
+                $line[] = $rowNum;
+                $line[] = $topRow ? 'TRUE' : '';
+
+                $line[] = isset($image) ? 'IMAGE' : '';
+                $line[] = isset($image) ? $image->src : '';
+                $line[] = isset($image) ? 'MERGE' : '';
+                $line[] = isset($image) ? $image->position : '';
+                $line[] = isset($image) ? $image->width : '';
+                $line[] = isset($image) ? $image->height : '';
+                $line[] = isset($image) ? $product->title : '';
+
+                $line[] = 'MERGE';
+                $line[] = isset($product->options[0]) ? $product->options[0]->name : '';
+                $line[] = $variant->option1;
+                $line[] = isset($product->options[1]) ? $product->options[1]->name : '';
+                $line[] = $variant->option2;
+                $line[] = isset($product->options[2]) ? $product->options[2]->name : '';
+                $line[] = $variant->option3;
+                $line[] = $variant->position;
+                $line[] = $variant->sku;
+                $line[] = $variant->barcode ? $variant->barcode : '';
+                $line[] = $variant->weight ? $variant->weight : '';
+                $line[] = $variant->weight_unit ? $variant->weight_unit : '';
+                $line[] = $vPrice;
+                $line[] = $variant->compare_at_price ? $variant->compare_at_price : '';
+                $line[] = 'TRUE';
+                $line[] = 'Shopify';
+                $line[] = $vInventoryPolicy;
+                $line[] = 'manual';
+                $line[] = 'TRUE';
+                $line[] = $vInventoryQuantity;
+                $line[] = 0;
+
+                fputcsv($file, $line);
+    
+                $i++;
+                $rowNum++;
+                $topRow = false;
+            }
+
+            // $cacheKey = 'incoming_product_' . $product->title;
+            // Cache::put($cacheKey, $product);
+
+            // ProcessIncomingProduct::dispatch($cacheKey, $filePath);
         }
+
+        fclose($file);
 
         Cache::forget($this->cacheKey);
     }
